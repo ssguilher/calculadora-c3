d3e7c73a694bf1129e2a4bc677f2e924c0787300
Guilherme <Guilherme@DESKTOP-VRMAJK1>
2022-12-10 14:14:53 -0300

    Adicionar arquivos de projeto.

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { getDefaultNormalizer } from '@testing-library/react';
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+const [nro1, setNro1] = useState(0);
+const [nro2, setNro2] = useState(0);
+const [resultado, setResultado] = useState(0);
+const [operacao, setOperacao] = useState('Somar');
+
+const calcular = () => {
+  if (operacao=="Somar")
+    return parseFloat(nro1) + parseFloat(nro2)
+    else if (operacao=="Subtrair")
+      return parseFloat(nro1) - parseFloat(nro2)
+    else if (operacao=="Multiplicar")
+      return parseFloat(nro1) * parseFloat(nro2)
+    else if (operacao=="Diviir")
+      return parseFloat(nro1) / parseFloat(nro2)
+}
+
+useEffect(()=>{
+  setResultado(calcular());
+}, [nro1, nro2, operacao]);
+
+  return (
+    <div className="App">
+      <h1>Calculadora React</h1>
+      <label>Primeiro Número</label>
+      <input
+      type="number" 
+      value={nro1}
+      onChange={(e)=> setNro1(e.target.value)} />
+
+      <label>Segundo Número</label>
+      <input
+      type="number" 
+      value={nro2}
+      onChange={(e)=> setNro2(e.target.value)} />
+
+      <select onChange={(e)=>setOperacao(e.target.value)}>
+        <option>Somar</option>
+        <option>Subtrair</option>
+        <option>Multiplicar</option>
+        <option>Dividir</option>
+      </select>
+
+      <label>Resultado {resultado}</label>
+    </div>
+  );
+}
+
+export default App;
